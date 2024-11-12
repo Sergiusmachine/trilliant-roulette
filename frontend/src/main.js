5b9073193b9c5ab12fb0900eab0da285ef5da4a9
Sergiusmachine <ser.01.182000@yandex.ru>
2024-11-12 22:03:14 +0300

    commit

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'https://unpkg.com/vue@3.5.6/dist/vue.esm-browser.js'
+import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
