97a7ef95f4f2979ca7c5a2d5c4dbb96d65cdfdd9
Sergiusmachine <ser.01.182000@yandex.ru>
2024-11-14 20:12:57 +0300

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm-bundler.js', // Это обязательное для правильного разрешения Vue в Vite
+    },
+  },
 })
