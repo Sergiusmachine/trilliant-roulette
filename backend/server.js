911e05924d95649363d34515e7918425d095a5b1
Sergiusmachine <ser.01.182000@yandex.ru>
2024-11-14 17:59:52 +0300

    commit

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const pool = new Pool({
 });
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://176.114.67.27']
+    origin: ['http://localhost:5173', 'http://176.114.67.27', 'http://trilliantroulette.ru', 'https://trilliantroulette.ru']
 }));
 app.use(json());
 
