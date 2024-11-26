ba8a3c1a1d52549b1c7635fd6374ce5a2b26be2c
Sergiusmachine <ser.01.182000@yandex.ru>
2024-11-26 23:49:49 +0300

    commit

--- frontend/src/pages/Roulette.vue
+++ frontend/src/pages/Roulette.vue
@@ -329,6 +329,11 @@ export default {
             this.timeLeft.hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
             this.timeLeft.minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
             this.timeLeft.seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+            if (timeLeft <= 0) {
+                this.timeLeft.hours = 0;
+                this.timeLeft.minutes = 0;
+                this.timeLeft.seconds = 0;
+            }
         },
 
         async getQuantity() {
