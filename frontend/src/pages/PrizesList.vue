67cd13f1cf6dcf993df5ea72ecde02a5b6e9fa0e
Sergiusmachine <ser.01.182000@yandex.ru>
2025-02-19 22:05:45 +0300

    commit

--- frontend/src/pages/PrizesList.vue
+++ frontend/src/pages/PrizesList.vue
@@ -439,7 +439,7 @@ export default {
         z-index: 2;
         width: 100%;
         height: 100vh;
-        background-color: rgba(0, 0, 0, 0.1);
+        background-color: rgba(0, 0, 0, 0.5);
         left: 50%;
         top: 50%;
         transform: translate(-50%, -50%);
