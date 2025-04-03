3aedbf9a89fb4e348cefe46741ddd732e181f2c3
Sergiusmachine <ser.01.182000@yandex.ru>
2025-04-03 18:17:26 +0300

    commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://trilliantroulett.ru/api",
+  baseURL: "https://trilliantroulette.ru/api",
   headers: {
     "Content-Type": "application/json",
   },
